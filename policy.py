20c9a512599ee98d27dbb762e14ad3756b8450cc
yizzy34 <30909832+yizzy34@users.noreply.github.com>
2023-04-23 16:09:05 -0500

    fixed policy.py

--- policy.py
+++ policy.py
@@ -1,18 +1,44 @@
 import random
 
 
-class Policy:
-    def __init__(self, agent, policy_type='PGREEDY'):
-        self.agent = agent
-        self.policy_type = policy_type
+def choose_Action_Based_On_Weight(actions, weights):
+    chosen_action_list = random.choices(actions, k=1, weights=weights)
+    position = random.randint(0, len(chosen_action_list[0]) - 1)
+    return chosen_action_list[0][position]
 
-    def set_policy_type(self, policy_type):
-        self.policy_type = policy_type
 
-    def choose_action(self, state, gender):
-        if self.policy_type == 'PRANDOM':
-            return random.choice(range(self.agent.n_actions))
-        elif self.policy_type == 'PGREEDY':
-            return self.epsilon_greedy_policy(state, gender)
-        elif self.policy_type == 'PEXPLOIT':
-            return self.exploit_policy(state)
+def p_Random(carrying, cells):
+    relevant_cells = [i for i in cells if
+                      (cells[i]['type'] == "pickup" and not carrying) or (cells[i]['type'] == "dropoff" and carrying)]
+    if relevant_cells:
+        return random.choice(relevant_cells)
+
+    return random.choice(list(cells))
+
+
+def p_Exploit(carrying, cells, q_vals):
+    relevant_cells = [i for i in cells if
+                      (cells[i]['type'] == "pickup" and not carrying) or (cells[i]['type'] == "dropoff" and carrying)]
+    if relevant_cells:
+        return random.choice(relevant_cells)
+
+    max_q_val = max(q_vals.values())
+    max_actions = [i for i in q_vals if q_vals[i] == max_q_val]
+    non_max_actions = [i for i in cells if i not in max_actions]
+
+    actions = [max_actions, non_max_actions]
+    weights = [0.80, 0.20]
+
+    return choose_Action_Based_On_Weight(actions, weights)
+
+
+def p_Greedy(carrying, cells, q_vals):
+    relevant_cells = [i for i in cells if
+                      (cells[i]['type'] == "pickup" and not carrying) or (cells[i]['type'] == "dropoff" and carrying)]
+    if relevant_cells:
+        return random.choice(relevant_cells)
+
+    max_q_val = max(q_vals.values())
+    max_actions = [i for i in q_vals if q_vals[i] == max_q_val]
+
+    return random.choice(max_actions)
