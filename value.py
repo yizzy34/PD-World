4dd5a909fbf561ce2bada341a56c2240c8a14938
yizzy34 <30909832+yizzy34@users.noreply.github.com>
2023-04-23 16:12:38 -0500

    fixed value.py

--- value.py
+++ value.py
@@ -1,6 +1,8 @@
 def q_Learning_Value_Function(old_q_val, move_reward, next_q_vals, alpha, gamma):
-    max_next_q_val = max(next_q_vals.values())
-    return old_q_val + alpha * (move_reward + gamma * max_next_q_val - old_q_val)
+    next_q_vals_list = []
+    for i in next_q_vals:
+        next_q_vals_list.append(next_q_vals[i])
+    return old_q_val + alpha * (move_reward + gamma * max(next_q_vals_list) - old_q_val)
 
 
 def sarsa_Value_Function(old_q_val, move_reward, next_q_val, alpha, gamma):
